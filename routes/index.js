dc5f54854e1d5bd19b7f0cc7ba72c25d55763c46
Jaqlig <74549994+jaqlig@users.noreply.github.com>
2021-11-17 19:54:23 +0100

    PoC frontend connection

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cleopatra' });
 });
 
+router.get('/express_backend', (req, res) => {
+  res.send({ express: 'NODEJS BACKEND CONNECTED TO REACT' });
+});
+
 module.exports = router;

8442d49c4e9c27a2501bea10262886b1cc9ac85a
jaqlig <74549994+jaqlig@users.noreply.github.com>
2021-11-21 15:58:45 +0100

    Minor model fixes

--- models/client.js
+++ models/client.js
@@ -5,6 +5,9 @@ var ClientSchema = new Schema(
   {
     first_name: {type: String, required: true, minlength: 1, maxlength: 100},
     last_name: {type: String, required: true, minlength: 1, maxlength: 100},
+    phone_number: {type: String, minlength: 1, maxlength: 20},
+    email: {type: String, minlength: 1, maxlength: 100},
+    password: {type: String, required: true},
     gender: {type: String, enum : ['male','female', 'other'], required: true},
     age: {type: String, enum : ['kid','young', 'mid', 'old'], required: true},
     hair_length: {type: String, minlength: 1, maxlength: 100},
@@ -15,11 +18,5 @@ var ClientSchema = new Schema(
     notes: {type: String}
   }
 );
-
-ClientSchema
-.virtual('url')
-.get(function () {
-    return '/Clients/' + this._id;
-});
 
 module.exports = mongoose.model('Client', ClientsSchema);
--- models/hairdresser.js
+++ models/hairdresser.js
@@ -6,6 +6,10 @@ var HairdresserSchema = new Schema(
     first_name: {type: String, required: true, minlength: 1, maxlength: 100},
     last_name: {type: String, required: true, minlength: 1, maxlength: 100},
     birth: {type: Date},
+    phone_number: {type: String, minlength: 1, maxlength: 20},
+    email: {type: String, minlength: 1, maxlength: 100},
+    password: {type: String, required: true},
+    employment_date: {type: Date, required: true},
     notes: {type: String}
   }
 );
